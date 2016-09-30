64bc16d70432d02c1f17ae1b0da86937b5bb246d
John Grossman <johngro@google.com>
2016-09-30 13:47:24 -0700

    [pixel2] Remove the pixel2 un-hide hacks.
    Remove the hacks which were put into place to un-hide the I2S and I2C
    controllers on Pixel 2.  Work on these subsystems has stopped as we
    move away from Pixel 2.  If (at some point) we want to go back to
    working on this, we should really deal with the hiding/unhiding in the
    proper place (e.g. the bootloader).  Failing that, the chipset config
    "driver" can be dug up and brought back to life up in user mode where
    it belongs.
    
    Change-Id: I1d6741d0420cdfd10386a6329de93dfab753f4f7

--- kernel/target/pc-x86/rules.mk
+++ kernel/target/pc-x86/rules.mk
@@ -14,7 +14,6 @@ PLATFORM := pc
 MODULE_SRCS += $(LOCAL_DIR)/config.c
 
 MODULE_DEPS += \
-    dev/broadwell_chipset_config \
     dev/i915 \
     dev/intel_rng \
     dev/thermal/intel_pch_thermal
--- kernel/target/pixel2/rules.mk
+++ kernel/target/pixel2/rules.mk
@@ -13,11 +13,9 @@ PLATFORM := pc
 
 MODULE_SRCS += \
 	$(LOCAL_DIR)/config.c \
-	$(LOCAL_DIR)/debug.c \
-	$(LOCAL_DIR)/pixel2_quirks.c
+	$(LOCAL_DIR)/debug.c
 
 MODULE_DEPS += \
-    dev/broadwell_chipset_config \
     dev/i915 \
     dev/intel_rng \
     dev/thermal/intel_pch_thermal
