394cb1d5e7c5d7ad89cc6da7ffdeb06f42a95d66
Christopher Anderson <cja@google.com>
2017-03-01 15:23:31 -0800

    [pci] Shift to using mx_pci_resource_t for memory
    PCI syscalls and protocols have been added to get pci_resource_t objects
    that encapsulate MMIO and PIO resources (Configs and BARs). In the MMIO
    case this means we're passing VMOs from the bus driver to users of the
    protocol. PIO is platform defined, but on x86 corresponds to an addr /
    size range.
    
    Change-Id: Ibcbd695efe3900316949adb837ee3c6a690c5802

--- system/ulib/ddk/include/ddk/protocol/pci.h
+++ system/ulib/ddk/include/ddk/protocol/pci.h
@@ -8,6 +8,7 @@
 #include <hw/pci.h>
 #include <magenta/compiler.h>
 #include <magenta/syscalls/pci.h>
+#include <magenta/types.h>
 
 __BEGIN_CDECLS;
 
@@ -17,6 +18,12 @@ __BEGIN_CDECLS;
  * The PCI host driver publishes mx_device_t's with its config set to a pci_device_config_t.
  */
 
+// This is used to handle calling map_resource without attempting to change
+// the cache policy when we know the bus driver has configured it already.
+#define PCI_CACHE_POLICY_BUS_DRIVER     (MX_CACHE_POLICY_MASK + 1u)
+// TODO: someday maybe we can centralize this
+#define ROUNDUP(a, b) (((a) + ((b)-1)) & ~((b)-1))
+
 typedef struct pci_protocol {
     mx_status_t (*claim_device)(mx_device_t* dev);
     mx_status_t (*map_mmio)(mx_device_t* dev,
@@ -25,6 +32,10 @@ typedef struct pci_protocol {
                             void** vaddr,
                             uint64_t* size,
                             mx_handle_t* out_handle);
+    mx_status_t (*map_resource)(mx_device_t* dev,
+                                mx_pci_resource_t* resource,
+                                uint32_t cache_policy,
+                                void** vaddr);
     mx_status_t (*enable_bus_master)(mx_device_t* dev, bool enable);
     mx_status_t (*enable_pio)(mx_device_t* dev, bool enable);
     mx_status_t (*reset_device)(mx_device_t* dev);
@@ -32,6 +43,11 @@ typedef struct pci_protocol {
     mx_status_t (*get_config)(mx_device_t* dev,
                               const pci_config_t** config,
                               mx_handle_t* out_handle);
+    mx_status_t (*get_config_ex)(mx_device_t* dev,
+                              mx_pci_resource_t* out_handle);
+    mx_status_t (*get_bar)(mx_device_t* dev,
+                              uint32_t bar_num,
+                              mx_pci_resource_t* out_bar);
     mx_status_t (*query_irq_mode_caps)(mx_device_t* dev,
                                        mx_pci_irq_mode_t mode,
                                        uint32_t* out_max_irqs);
