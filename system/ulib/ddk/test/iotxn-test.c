7db5e97eecc4be51764a7e38488200d74fef9db6
Yvonne Yip <yky@google.com>
2017-04-10 14:59:23 -0700

    [ddk][test] driver testing
    TODO: edit this message
    
    Add simple framework for unit testing in driver space. Test
    drivers are not autobindable, and implements MX_PROTOCOL_TEST.
    On boot, the system creates the /dev/misc/test device which is
    the parent of all test drivers. Add a new device ioctl,
    IOCTL_DEVICE_RUN_TESTS, to run tests from /dev/misc/test. The
    /boot/test/driver-tests program, will bind test drivers to
    /dev/misc/tests and run tests.
    
    Because driver-tests is invoked by runtests, the tests it
    run should not have side effects!
    
    Move utest/core/iotxn-test to a test driver and add a
    contiguous allocation test.
    
    Change-Id: I2a4848a82143aaf4dbb97f7b64df6a8dad88dcf6

--- system/ulib/ddk/test/iotxn-test.c
+++ system/ulib/ddk/test/iotxn-test.c
@@ -1,8 +1,12 @@
-// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <ddk/device.h>
+#include <ddk/driver.h>
+#include <ddk/binding.h>
 #include <ddk/iotxn.h>
+#include <ddk/protocol/test.h>
 
 #include <unittest/unittest.h>
 #include <stddef.h>
@@ -20,6 +24,19 @@ static bool test_physmap_simple(void) {
     ASSERT_NONNULL(txn->phys, "expected phys to be set");
     ASSERT_EQ(txn->phys_offset, 0u, "unexpected phys_offset");
     ASSERT_EQ(txn->phys_length, 3u, "unexpected phys_length");
+    iotxn_release(txn);
+    END_TEST;
+}
+
+static bool test_physmap_contiguous(void) {
+    BEGIN_TEST;
+    iotxn_t* txn;
+    ASSERT_EQ(iotxn_alloc(&txn, IOTXN_ALLOC_CONTIGUOUS, PAGE_SIZE * 3), NO_ERROR, "");
+    ASSERT_NONNULL(txn, "");
+    ASSERT_EQ(iotxn_physmap(txn), NO_ERROR, "");
+    ASSERT_NONNULL(txn->phys, "expected phys to be set");
+    ASSERT_EQ(txn->phys_offset, 0u, "unexpected phys_offset");
+    ASSERT_EQ(txn->phys_length, 1u, "unexpected phys_length");
     iotxn_release(txn);
     END_TEST;
 }
@@ -91,14 +108,37 @@ static bool test_physmap_unaligned_offset2(void) {
 
 BEGIN_TEST_CASE(iotxn_tests)
 RUN_TEST(test_physmap_simple)
+RUN_TEST(test_physmap_contiguous)
 RUN_TEST(test_physmap_clone)
 RUN_TEST(test_physmap_aligned_offset)
 RUN_TEST(test_physmap_unaligned_offset)
 RUN_TEST(test_physmap_unaligned_offset2)
 END_TEST_CASE(iotxn_tests)
 
-#ifndef BUILD_COMBINED_TESTS
-int main(int argc, char** argv) {
-    return unittest_run_all_tests(argc, argv) ? 0 : -1;
+static mx_status_t iotxn_test_func(void* cookie, test_report_t* report, const void* arg, size_t arglen) {
+    // TODO: write the report
+    return unittest_run_all_tests(0, NULL);
 }
-#endif
+
+static mx_status_t iotxn_test_bind(mx_driver_t* drv, mx_device_t* dev, void** cookie) {
+    printf("iotxn_test_bind: dev %p(%s)\n", dev, dev->name);
+    test_protocol_t* protocol;
+    mx_status_t status = device_get_protocol(dev, MX_PROTOCOL_TEST, (void**)&protocol);
+    if (status != NO_ERROR) {
+        return status;
+    }
+
+    protocol->set_test_func(dev, iotxn_test_func, NULL);
+    return NO_ERROR;
+}
+
+mx_driver_t _driver_iotxn_test = {
+    .ops = {
+        .bind = iotxn_test_bind,
+    },
+};
+
+MAGENTA_DRIVER_BEGIN(_driver_iotxn_test, "iotxn-test", "magenta", "0.1", 2)
+    BI_ABORT_IF_AUTOBIND,
+    BI_MATCH_IF(EQ, BIND_PROTOCOL, MX_PROTOCOL_TEST),
+MAGENTA_DRIVER_END(_driver_iotxn_test)
